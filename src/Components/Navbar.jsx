af7582bd3f8f6f751bd24f7668dde8687c7b3dba
Madhur <bhartimadhur21@gmail.com>
2023-08-04 11:46:29 +0530

    navbar responsiveness

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,27 @@
 import React, {useState} from 'react';
 import styles from '../Styles/navbar.module.scss'
+import {AiOutlineMenu} from 'react-icons/ai'
 function Navbar(props) {
+    const [click,setClick]=useState(false);
     return (
         <>
-            <div className={styles.navBox}>
+            <div className={ click===true?styles.navBoxResp: styles.navBox}>
                 <div>
                     <img  className={styles.logo}  src='/public/hireMe.png'/>
                 </div>
-                <div className={styles.navLinkBox}>
+                <div className={ click===true? styles.navLinkBoxResp : styles.navLinkBox}>
                     <div className={styles.navItems}>About</div>
                 <div className={styles.navItems}>Product</div>
                 <div className={styles.navItems}>Customers</div>
                     <div className={styles.navItems}>Resources</div>
                 <div className={styles.navItems}>Pricing</div>
                 </div>
-                <div className={styles.callToAction}>
+                <div className={ click===true? styles.callToActionResp : styles.callToAction}>
                     <button className={styles.loginBtn}>Login</button>
                     <button className={styles.signupBtn}>Signup</button>
                 </div>
+                <AiOutlineMenu className={styles.menuIcon} onClick={()=>setClick(!click)} />
+
             </div>
         </>
     );
